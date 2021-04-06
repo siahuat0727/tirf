76476f82f24ed8251abaeb30945f4492098c481d
siahuat0727 <tansiahuat@gmail.com>
2021-04-06 08:52:35 +0800

    Refactor

--- utils/utils.py
+++ utils/utils.py
@@ -6,7 +6,7 @@ class Tirf:
     def __init__(self, path):
         self.cap = cv2.VideoCapture(path)
         assert self.cap.isOpened(), \
-                f'Check if the video path is correct: {path}'
+            f'Check if the video path is correct: {path}'
 
     @property
     def fps(self):
@@ -17,12 +17,14 @@ class Tirf:
         for i in count(0):  # Infinite loop
             ret, frame = self.cap.read()
             if not ret:
-                self.cap.release()
-                return
+                break
 
             if i == 0:
                 assert frame.ndim == 3, \
-                        f'Espected video in 3 chans but {frame.ndim}'
+                    f'Espected video in 3 chans but {frame.ndim}'
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
+
             yield i, frame
+
+        self.cap.release()
